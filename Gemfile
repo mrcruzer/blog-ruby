66fc42a7f20aff7a0a9789ac07c4afb3c7dcec5c
mrcruzer <isaacavelino14@gmail.com>
2018-10-26 18:11:51 -0400

    Update-4

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.5.1'
 
 # iconv
 gem "iconv", "~> 1.0.3"
+# Boostrap
+gem 'bootstrap', '~> 4.1.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  root 'articles#index'
   # ruta que cargara por defecto
   
   get 'welcome/contactos'
